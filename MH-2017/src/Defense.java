ce71dc5b9b0252f9ac3c425cf60a68677b872f16
Tim Callies <timcallies@gmail.com>
2018-01-28 01:00:07 -0600

    -Birthed

---
+++ MH-2017/src/Defense.java
@@ -0,0 +1,13 @@
+/*
+    This class defines a defensive formation.
+ */
+public class Defense extends Formation
+{
+    
+    public Defense(Team team)
+    {
+        super(team);
+    }
+    
+    
+}
