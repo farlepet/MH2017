1e94ed55209151bc06b05954f0f1b634ce54d004
Tim Callies <timcallies@gmail.com>
2018-01-27 21:32:57 -0600

    Created team class

--- MH-2017/src/Team.java
+++ MH-2017/src/Team.java
@@ -1,13 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    This abstract class contains a list of players and their positions.
+
+    Instance Data:
+        roster: An arraylist which contains a list of all the players on a team
+        along with their position.
  */
 
-/**
- *
- * @author timca
- */
-public class Team {
+import java.util.ArrayList;
+
+public abstract class Team
+{
+    private ArrayList<PlayerEntry> roster;
     
+    
+    private class PlayerEntry
+    {
+        
+    }
 }
