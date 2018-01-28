7a45d200a7fb8f65d5143ecb9ee69276725d9ee3
Mitchell Nelson <MitchellNelson@guest-10-162-195-241.wireless.umn.edu>
2018-01-28 01:27:41 -0600

    Driver tests

--- MH-2017/src/Driver.java
+++ MH-2017/src/Driver.java
@@ -4,28 +4,62 @@ public class Driver
 
 	public static void main(String[] args)
 	{
-
-		Injury myInjury;
-
-		myInjury = new Injury(31,12,2017, 2);
-
-		Player myPlayer;
-
-		myPlayer = new Player();
-
-		myPlayer.setName("Mitchll");
-
-		myPlayer.setHeight (213);
-
-		myPlayer.addInjury(myInjury);
-
-		System.out.println (myInjury.daysSinceInjury());
-
 		Team myTeam;
 
-		myTeam= new Team();
+		myTeam= new Team("Jarvis Knights");
 
-		myTeam.addPlayer(myPlayer);
+		Player mitchell;
+
+		Player riley;
+
+		Player tim;
+
+		Player pete;
+
+		mitchell = new Player();
+
+		riley = new Player();
+
+		tim = new Player();
+
+		pete = new Player();
+
+		Injury mInjury;
+
+		Injury rInjury;
+
+		Injury tInjury;
+
+		Injury pInjury;
+
+		mInjury = new Injury(2,1,2018, 0); // head injury
+
+		rInjury = new Injury(2,1,2018, 1); // torso injury
+
+		tInjury = new Injury(3,1,2018, 2); // arm injury
+
+		pInjury = new Injury(4,1,2018, 3); // leg injury
+
+		mitchell.addInjury(mInjury);
+
+		riley.addInjury(rInjury);
+
+		tim.addInjury(tInjury);
+
+		pete.addInjury(pInjury);
+
+		
+
+		System.out.println(tim.riskAgainst(riley));
+		//System.out.println(riley.riskFactorAgainst());
+
+		//System.out.println(tim.riskFactorAgainst());
+
+		//System.out.println(pete.riskFactor());
+
+
+
+
 
 	}
 }
--- MH-2017/src/Player.java
+++ MH-2017/src/Player.java
@@ -10,7 +10,11 @@ public class Player
 
 	 public int height;
 
+	 public int weight;
+
 	 public ArrayList<Injury> injuryHistory;
+
+	 public Injury currentInjury;
 
 	 public Player ()
 	 {
@@ -22,6 +26,74 @@ public class Player
 	 	injuryHistory.add (injury);
 	 }
 
+	 public double riskAgainst(Player opponent)
+	 {
+	 	double riskFactor = 0;
+
+	 	for (int i =0; i <injuryHistory.size();i++)
+	 	{
+	 		currentInjury = injuryHistory.get(i);
+
+	 		riskFactor += injuryFormula(currentInjury);
+	 	}
+
+
+
+	 	if( riskFactor > 100)
+	 	{
+	 		riskFactor = 100;
+	 	}
+
+
+
+	 	return riskFactor;
+	 }
+
+	 private double weightFormula (  )
+
+	 private double injuryFormula(Injury injury)
+	 {
+	 	double retVal;
+
+	 	int type;
+
+	 	int a,b;
+
+	 	type = injury.getType();
+
+	 	/*
+			Head injuries put Players at much greater risk of a further head injury, 
+			no matter the revoery time. A leg injury takes longer to recover than most. 
+	 	*/
+
+	 	if ( type == 0 )//if injury is head injury
+	 	{
+	 		a = 200;
+
+	 		b=15; // Head injuries will never bring risk to 0
+	 	}
+
+	 	else if ( type == 3)//if injury is leg injury 
+	 	{
+
+	 		a = 500;
+
+	 		b = 0; // Leg injuries 
+
+	 	}
+
+	 	else // all other types of injuries
+	 	{
+	 		a = 100;
+
+	 		b = 0;
+	 	}
+	 	System.out.println("a = " + a + "  b = " + b);
+	 	retVal = ((double)a/(double)injury.daysSinceInjury())+(double)b;
+
+	 	return retVal;
+	 }
+
 	 public void setName(String name) {this.name = name;}
 
 	 public void setNumber(int number){this.number = number;}
@@ -30,6 +102,8 @@ public class Player
 
 	 public void setHeight(int height) {this.height= height;}
 
+	 public void setWeight(int weight) {this.weight= weight;}
+
 	 public String getName() {return this.name;}
 
 	 public int getNumber() {return this.number;}
@@ -37,5 +111,7 @@ public class Player
 	 public int getAge() {return this.age;}
 
 	 public int getHeight() {return this.height;}
+
+	 public int getWeight() {return this.weight;}
 
 }
